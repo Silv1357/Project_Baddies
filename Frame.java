9ed1073c24be618137e791035e34cd95812c063b
karla2prado5 <117671730+karla2prado5@users.noreply.github.com>
2022-11-10 11:18:02 -0700

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,158 @@
+
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// class with buttons
+public class Frame{
+  public static void main (String[] args){
+
+    Button butt = new Button();
+    butt.button();
+
+}
+
+// public static void map(){
+//   // create a JFrame
+//   JFrame a = new JFrame();
+//   a.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+//   a.setSize (800, 500); // sets size of frame
+//
+//   // scale image
+//   final ImageIcon originalImage = new ImageIcon ("map2.jpeg");
+//   // original image dimensions 1011x742
+//   final ImageIcon scaledImage = new ImageIcon (originalImage.getImage().getScaledInstance (761, 492, Image.SCALE_SMOOTH));
+//
+//   // create a JLabel
+//   JLabel label = new JLabel (scaledImage);
+//   Dimension size = label.getPreferredSize();
+//   label.setPreferredSize (new Dimension (250, 100));
+//
+//   a.add(label); // add label (image) to frame
+//   a.setVisible (true);
+// }
+}
+//
+//
+//   // Athletics and Sports Button
+//   JButton sports = new JButton ("Sports");
+//   sports.setBounds (30, 200, 100, 25);
+//   a.add(b);
+//   a.setLayout (null);
+//   a.setVisible(true);
+//   sports.addActionListener(this);
+//
+//   // Language House Button
+//   JButton languageHouses = new JButton ("Language Houses");
+//   languageHouses.setBounds (200, 20, 150, 25);
+//   a.add(c);
+//   a.setLayout (null);
+//   a.setVisible(true);
+//   languageHouses.addActionListener(this);
+//
+//   // West Campus Button
+//   JButton westCampus = new JButton ("West Campus");
+//   westCampus.setBounds (150, 175, 120, 25);
+//   a.add (d);
+//   a.setLayout (null);
+//   a.setVisible(true);
+//   westCampus.addActionListener(this);
+//
+//   // Worner
+//   JButton worner = new JButton ("Worner");
+//   worner.setBounds (240, 300, 100, 25);
+//   a.add (e);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   worner.addActionListener(this);
+//
+//   // Music and Theatre
+//   JButton musicAndTheatre = new JButton ("Music & Theatre");
+//   musicAndTheatre.setBounds (300, 375, 130, 25);
+//   a.add (f);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   musicAndTheatre.addActionListener(this);
+//
+//   // Tutt Library
+//   JButton tuttLibrary = new JButton ("Tutt Library");
+//   tuttLibrary.setBounds (350, 130, 100, 25);
+//   a.add (g);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   tuttLibrary.addActionListener(this);
+//
+//   // Armstrong
+//   JButton armstrong = new JButton ("Armstrong");
+//   armstrong.setBounds (375, 300, 100, 25);
+//   a.add (h);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   armstrong.addActionListener(this);
+//
+//   // South Hall
+//   JButton southHall = new JButton ("South Hall ");
+//   southHall.setBounds (525, 300, 100, 25);
+//   a.add (i);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   southHall.addActionListener(this);
+//
+//   // Ed Robson
+//   JButton edRobson = new JButton ("Ed Robson ");
+//   edRobson.setBounds (510, 380, 100, 25);
+//   a.add (j);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   edRobson.addActionListener(this);
+//
+//   // Shove Chapel
+//   JButton shove = new JButton ("Shove Chapel");
+//   shove.setBounds (510, 220, 110, 25);
+//   a.add (k);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   shove.addActionListener(this);
+//
+//   // Class
+//   JButton classes = new JButton ("Class");
+//   classes.setBounds (500, 130, 100, 20);
+//   a.add (l);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   classes.addActionListener(this);
+//
+//   // Mathias
+//   JButton mathias = new JButton ("Mathias");
+//   mathias.setBounds (530, 30, 75, 20);
+//   a.add (m);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   mathias.addActionListener(this);
+//
+//   // East Campus
+//   JButton eastCampus = new JButton ("East Campus ");
+//   eastCampus.setBounds (650, 150, 120, 25);
+//   a.add (n);
+//   a.setLayout(null);
+//   a.setVisible(true);
+//   eastCampus.addActionListener(this);
+//
+//   // create a new JPanel
+//   JPanel panel = new JPanel (new BorderLayout());
+//   panel.setSize (800, 500);
+//
+//   panel.add (label);
+//   a.add(panel);
+//
+// }
+//   public void actionPerformed(ActionEvent e){
+//     if(e.getSource() == //button variable name) {
+//       //print something
+//       //Scanner
+//       //take scanner input
+//     }
+//   }
+// }
