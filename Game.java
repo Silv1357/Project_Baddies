53615a67ded6c417b872d8ec94a3924bf5dcb1dc
karla2prado5 <117671730+karla2prado5@users.noreply.github.com>
2022-11-14 10:30:08 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,8 +1,10 @@
 //for our Game class
-// runs Player class
-
+// runs Player and Map class
 import java.util.*;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game{
 
@@ -10,13 +12,8 @@ private Map coloradoCollege;
 private int healthScore;
 
   public static void main(String[] args){
-
-    System.out.println( "Nick: Welcome to Colorado College; unfortunately, you have come at a really bad time. \nSquirrels are attacking everyone and they all have rabies. We need the special elixir to cure CC. \nI need to find my special elixir or the world is doomed; \nIt is somewhere on campus and you're going to have to help me \nFirst, what is your name?\n");
-
-    System.out.println("Enter your name: ");
-    Scanner userInput = new Scanner(System.in);
-    String name = userInput.nextLine();
-    System.out.println("Welcome, " + name +". \nYou have to go around campus, visiting the different locations. \nIn each, you will be faced with 2 choices. \nIf you make the right choice, you will get a clue to find the elixir. \nBut if you make the wrong choice, you will be attacked by a squirrel and get -20 health; \n5 wrong choices will kill you");
+    JOptionPane.showMessageDialog(null, "In this game, you will walk around visiting different parts of Colorado College, \nYou will be faced with 2 options at each location: \nIf you choose correctly you get a clue to find the elixir \nbut if you choose the wrong option, the rabid squirrels will attack you and deduct 20 health from you \nFind the elixir before your health gets to 0 to save CC!", "Instructions", JOptionPane.PLAIN_MESSAGE);
+    JOptionPane.showMessageDialog(null, "Welcome to Colorado College; unfortunately, you have come at a really bad time. \nSquirrels are attacking everyone and they all have rabies. We need the special elixir to cure CC. \nI need to find my special elixir or the world is doomed; it is somewhere on campus and you're going to have to help me! \nHint: Start at Worner ", "Welcome!!", JOptionPane.PLAIN_MESSAGE);
 
   }
 
