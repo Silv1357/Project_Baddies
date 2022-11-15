66bebf8176a554842b3e66c88f564a41da8e74da
karla2prado5 <117671730+karla2prado5@users.noreply.github.com>
2022-11-15 16:22:30 -0700

    Update Squirrel.java

--- Squirrel.java
+++ Squirrel.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+//this abstract class serves to assign an action to a squirrel then retrieve the action when we call the getAction method 
 public abstract class Squirrel{
   private String action;
 
