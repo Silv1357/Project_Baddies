bf5461da8b309881cfc003bb08b531db7f328cc8
karla2prado5 <117671730+karla2prado5@users.noreply.github.com>
2022-11-15 16:22:55 -0700

    Update RabidSquirrel.java

--- RabidSquirrel.java
+++ RabidSquirrel.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+//this class inherits from Squirrels and by saying super.SquirrelAction we assign the different actions to the squirrel later used in the Button class 
 public class RabidSquirrel extends Squirrel{
   public void setAction(String action){
     super.squirrelAction(action);
