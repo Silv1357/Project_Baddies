3a40b59f2609d8b043d13070f32c12aeaf0181c1
Silv1357 <116597286+Silv1357@users.noreply.github.com>
2022-11-15 21:24:26 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*; // Files
 
-//in this class we will having the openign frame asking the user to start the game, and it will have pop ups explaining the game and the rules before actually running the game 
+// in this class we will having the opening frame asking the user to start the game, and a pop up will appear, explaining the game and the rules before actually running the game 
 public class Game{
 
   public static void main(String[] args) throws FileNotFoundException{
